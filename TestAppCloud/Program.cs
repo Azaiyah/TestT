44d7f8f31a70499fcc820f69bed5e9fe66aafb32
Azaiyah L <emrahhuremagic18@gmail.com>
2025-03-13 11:31:37 +0000

    Create menu design - no function

--- TestAppCloud/Program.cs
+++ TestAppCloud/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void Menu()
+{
+    Console.WriteLine("------------------------------------------");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Substract");
+    Console.WriteLine("3. Multiply");
+    Console.WriteLine("4. Divide");
+    Console.WriteLine("------------------------------------------");
+}
+
+
+Menu();
--- TestAppCloud/obj/Debug/net8.0/TestAppCloud.AssemblyInfo.cs
+++ TestAppCloud/obj/Debug/net8.0/TestAppCloud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAppCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07148b872467ae7771cb61b49e60d9c92a65d4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4220bb78294b24812a5c0a64be04b4edb463bc20")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAppCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAppCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
