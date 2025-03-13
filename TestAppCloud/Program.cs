fbb9881c75a74b94867beb493f35abe4dcb1d5c9
Azaiyah L <emrahhuremagic18@gmail.com>
2025-03-13 10:36:24 +0000

    First test commit GitPod

---
+++ TestAppCloud/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+TestMethod();
+
+Console.WriteLine("Hello, World!");
+
+
+
+ void TestMethod()
+{
+    Console.WriteLine("Test output");
+}
+
+TestMethod();
